8e54678df366d6de711b01124f22fd70b0298346
Vitoria <168016396+uaivitoria@users.noreply.github.com>
2024-09-03 16:24:02 -0300

    Add files via upload

---
+++ todolist/src/main/java/br/com/cursojava/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.cursojava.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+
+    
+}
